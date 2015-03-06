a676f52b9b91b53aa962b0e883d6ba5fad3f3470
m85091081 <b10216041@yuntech.edu.tw>
2015-03-07 00:58:01 +0800

    ADD
    ADD

---
+++ js/youtube.js
@@ -0,0 +1,21 @@
+						var tag = document.createElement('script');
+						tag.src = "https://www.youtube.com/player_api";
+						var firstScriptTag = document.getElementsByTagName('script')[0];
+						firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+						// Replace the 'ytplayer' element with an <iframe> and
+						// YouTube player after the API code downloads.
+						var player;
+						function onYouTubePlayerAPIReady() {
+						player = new YT.Player('ytplayer', {
+						height: '480',
+						width: '860' ,
+						 playerVars: 
+						 {
+							 listType:'playlist',
+							 list: 'PLPdK88gZ5i6R8P-GDI50jHIirha91hsNm',
+							 autoplay:1
+							
+							}
+
+    					});
+  						}
